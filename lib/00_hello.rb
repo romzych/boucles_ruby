5ec7c0d3b00e368c4320848f6198ba083531c411
Roman Blondel <rblondel23@gmail.com>
2019-10-03 11:08:26 +0200

    fix ex 1

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -5,8 +5,8 @@ def ask_first_name
 end
 
 def say_hello(first_name)
-    return "Bonjour, #{first_name}!"
+    puts "Bonjour, #{first_name}!"
 end
 
 first_name = ask_first_name
-puts say_hello(first_name)
+say_hello(first_name)
